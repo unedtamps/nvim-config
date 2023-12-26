de908c24639a3bb0366ea83b4ab1fa4c7ee44a7e
Unedo Tampubolon <unedo.tampubolon@gmail.com>
2023-12-26 22:21:59 +0700

    terminal lua

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -3,9 +3,40 @@ return {
 		"akinsho/toggleterm.nvim",
 		tag = "*",
 		config = function()
-			require("toggleterm").setup()
-			vim.keymap.set("n", "<c-T>", ":ToggleTerm size=10 dir=./ direction=horizontal<CR>")
-			vim.keymap.set("t", "<c-T>", "exit<CR>")
+			require("toggleterm").setup({
+				size = 60,
+				open_mapping = "T",
+				hide_numbers = true,
+				shade_filetypes = {},
+				shade_terminals = true,
+				shading_factor = 2,
+				start_in_insert = true,
+				insert_mapping = true,
+				persist_size = true,
+				direction = "vertical",
+				close_on_exit = true,
+				shell = vim.o.shell,
+				float_opts = {
+					border = "curved",
+					winblend = 0,
+					highlights = {
+						border = "Normal",
+						background = "Normal",
+					},
+				},
+			})
+
+			function _G.set_terminal_keymaps()
+				local opts = { buffer = 0 }
+				vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+				vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+				vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+				vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+				vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+				vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+				vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
+			end
+			vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
 		end,
 	},
 }
