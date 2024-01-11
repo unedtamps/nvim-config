dcbabcbcff380e6b65a7cde89654196c99b765f9
Unedo Tampubolon <unedo.tampubolon@gmail.com>
2024-01-11 13:45:18 +0700

    change colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,58 +1,45 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
+
+	"folke/tokyonight.nvim",
+	lazy = false,
 	priority = 1000,
+	opts = {},
 	config = function()
-		require("catppuccin").setup({
-			flavour = "frappe", -- latte, frappe, macchiato, mocha
-			background = { -- :h background
-				light = "latte",
-				dark = "mocha",
-			},
-			transparent_background = true, -- disables setting the background color.
-			show_end_of_buffer = true, -- shows the '~' characters after the end of buffers
-			term_colors = true, -- sets terminal colors (e.g. `g:terminal_color_0`)
-			dim_inactive = {
-				enabled = false, -- dims the background color of inactive window
-				shade = "dark",
-				percentage = 0.15, -- percentage of the shade to apply to the inactive window
-			},
-			no_italic = false, -- Force no italic
-			no_bold = false, -- Force no bold
-			no_underline = false, -- Force no underline
-			styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-				comments = { "italic" }, -- Change the style of comments
-				conditionals = {},
-				loops = {},
-				functions = { "italic", "bold" },
-				keywords = { "bold" },
-				strings = {},
+		require("tokyonight").setup({
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+			light_style = "day", -- The theme is used when the background is set to light
+			transparent = true, -- Enable this to disable setting the background color
+			terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
+			styles = {
+				-- Style to be applied to different syntax groups
+				-- Value is any valid attr-list value for `:help nvim_set_hl`
+				comments = { italic = true },
+				keywords = { bold = true },
+				functions = { italic = true, bold = true },
 				variables = {},
-				numbers = {},
-				booleans = { "bold" },
-				properties = {},
-				types = { "bold" },
-				operators = {},
+				-- Background styles. Can be "dark", "transparent" or "normal"
+				sidebars = "transparent", -- style for sidebars, see below
+				floats = "transparent", -- style for floating windows
 			},
-			color_overrides = {},
-			custom_highlights = {},
-			integrations = {
-				cmp = true,
-				gitsigns = true,
-				nvimtree = true,
-				dashboard = true,
-				treesitter = true,
-				harpoon = true,
-				alpha = true,
-				telescope = true,
-				notify = false,
-				mini = {
-					enabled = true,
-					indentscope_color = "",
-				},
-				-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-			},
+			sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+			day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+			hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+			dim_inactive = false, -- dims inactive windows
+			lualine_bold = true, -- When `true`, section headers in the lualine theme will be bold
+
+			--- You can override specific color groups to use other groups or a hex color
+			--- function will be called with a ColorScheme table
+			---@param colors ColorScheme
+			on_colors = function(colors) end,
+
+			--- You can override specific highlights to use other groups or a hex color
+			--- function will be called with a Highlights and ColorScheme table
+			---@param highlights Highlights
+			---@param colors ColorScheme
+			on_highlights = function(highlights, colors) end,
 		})
-		vim.cmd.colorscheme("catppuccin")
+		vim.cmd.colorscheme("tokyonight-storm")
 	end,
 }
