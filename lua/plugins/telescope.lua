54a75c9ace56862c45272f08874b8948e037f6aa
Unedo Tampubolon <unedo.tampubolon@gmail.com>
2024-01-27 15:38:06 +0700

    add new builtin to telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,6 +7,9 @@ return {
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+			vim.keymap.set("n", "<leader>pg", builtin.git_files, {})
+			vim.keymap.set("n", "<leader>pb", builtin.buffers, {})
+			vim.keymap.set("n", "<leader>ps", builtin.grep_string, {})
 		end,
 	},
 	{
