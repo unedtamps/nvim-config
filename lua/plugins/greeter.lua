a5ed10d42c520e3ee7a6551963370be2b50251ae
Unedo Tampubolon <unedo.tampubolon@gmail.com>
2024-01-29 03:40:20 +0700

    config indent and greater

--- lua/plugins/greeter.lua
+++ lua/plugins/greeter.lua
@@ -1,7 +1,59 @@
 return {
-	"goolord/alpha-nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
+	"nvimdev/dashboard-nvim",
+	event = "VimEnter",
+	indent = false,
 	config = function()
-		require("alpha").setup(require("alpha.themes.startify").config)
+		require("dashboard").setup({
+			theme = "hyper",
+			hide = {
+				statusline = true,
+				tabline = true,
+				winbar = true,
+			},
+			preview = {
+				file_width = true,
+			},
+			config = {
+				header = {
+					[[                                                                       ]],
+					[[                                                                       ]],
+					[[                                                                       ]],
+					[[                                                                       ]],
+					[[                                                                     ]],
+					[[       ████ ██████           █████      ██                     ]],
+					[[      ███████████             █████                             ]],
+					[[      █████████ ███████████████████ ███   ███████████   ]],
+					[[     █████████  ███    █████████████ █████ ██████████████   ]],
+					[[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+					[[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+					[[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+					[[                                                                       ]],
+					[[                                                                       ]],
+					[[                                                                       ]],
+				},
+				week_header = {
+					enable = false,
+				},
+				shortcut = {
+					{ icon = "⟳ ", desc = "Update", group = "@property", action = "Lazy update", key = "u" },
+					{
+						icon = " ",
+						icon_hl = "@variable",
+						desc = "Files",
+						group = "Label",
+						action = "Telescope find_files",
+						key = "f",
+					},
+					{
+						icon = " ",
+						desc = "String",
+						group = "DiagnosticHint",
+						action = "Telescope grep_string",
+						key = "s",
+					},
+				},
+			}, -- config
+		})
 	end,
+	dependencies = { { "nvim-tree/nvim-web-devicons" } },
 }
