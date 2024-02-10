ba4d220268c745e7de14821d5883a1191a172963
Unedo Tampubolon <unedo.tampubolon@gmail.com>
2024-02-11 00:07:37 +0700

    add code runner and debugger

---
+++ lua/plugins/coderunner.lua
@@ -0,0 +1,53 @@
+return {
+	{
+		"michaelb/sniprun",
+		branch = "master",
+
+		build = "sh install.sh",
+
+		config = function()
+			require("sniprun").setup({})
+			vim.keymap.set("n", "<leader>sp", "<cmd>SnipRun<CR>")
+			vim.keymap.set("v", "<leader>sp", "<cmd>SnipRun<CR>")
+		end,
+	},
+	{
+		"CRAG666/code_runner.nvim",
+		config = function()
+			require("code_runner").setup({
+				filetypes = {
+					javascript = "nodemon index.js",
+					typescript = "nodemon index.ts",
+					python = "python $filename",
+					c = {
+						"gcc -o $filenameWithoutExt $filename &&",
+						"./$filenameWithoutExt",
+					},
+					cpp = {
+						"g++ -o $filenameWithoutExt $filename &&",
+						"./$filenameWithoutExt",
+					},
+				},
+				startinsert = true,
+				mode = "float",
+				float = {
+					close_key = "<ESC>",
+					border = "double",
+					width = 0.5,
+					height = 0.6,
+					border_hl = "FloatBorder",
+					float_hl = "Normal",
+				},
+				term = {
+					position = "bot",
+					size = 10,
+					direction = "horizontal",
+				},
+				before_run_filetype = function()
+					vim.cmd(":w")
+				end,
+			})
+			vim.keymap.set("n", "<C-n>", "<cmd>RunCode<CR>")
+		end,
+	},
+}
